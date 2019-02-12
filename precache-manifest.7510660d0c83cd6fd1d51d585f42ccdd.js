dc3454114bebd86ef99775c714ec03362f705b41
jsbimra <jsbimra@users.noreply.github.com>
2019-02-13 01:37:13 +0530

    Updates

---
+++ precache-manifest.7510660d0c83cd6fd1d51d585f42ccdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ca242ec2ab595d5f3c2",
+    "url": "/oddle-test/static/js/runtime~main.7ca242ec.js"
+  },
+  {
+    "revision": "22605a3b999d5d601acb",
+    "url": "/oddle-test/static/js/main.22605a3b.chunk.js"
+  },
+  {
+    "revision": "331316496aa6ad4853db",
+    "url": "/oddle-test/static/js/2.33131649.chunk.js"
+  },
+  {
+    "revision": "22605a3b999d5d601acb",
+    "url": "/oddle-test/static/css/main.40b62c84.chunk.css"
+  },
+  {
+    "revision": "6ada94ce6d1e90bde1ddeab392b5ca2e",
+    "url": "/oddle-test/index.html"
+  }
+];
